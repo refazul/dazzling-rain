d97e6b5e6044fee27d155a96305bcded6ecf10fe
Refazul <refazul.refat@yahoo.com>
2022-10-10 01:59:24 +0600

    CSRF_TRUSTED_ORIGINS

--- projectname/settings.py
+++ projectname/settings.py
@@ -136,3 +136,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['http://business.cotfield.com']
