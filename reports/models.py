c982d80833739883b4b16c30bce826d8e1ee62fb
Refazul <refazul.refat@yahoo.com>
2022-10-10 14:59:31 +0600

    police model daates

--- reports/models.py
+++ reports/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-import datetime
+from datetime import date, datetime, timedelta
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
     return 'police_{0}/{1}'.format(instance.police_id, filename)
+
+def get_default_my_date():
+  return date.today() - timedelta(days=25)
 
 class PoliceRanks(models.TextChoices):
     IGP = 'IGP', _('আইজিপি')
@@ -23,14 +26,19 @@ class PoliceRanks(models.TextChoices):
 
 # Create your models here.
 class Police(models.Model):
-    police_id = models.CharField(max_length=50, primary_key = True)
-    police_name_english = models.CharField(max_length=200)
-    police_name_bangla = models.CharField(max_length=200)
-    police_rank = models.CharField(
-        max_length=4,
-        choices=PoliceRanks.choices,
-        default=PoliceRanks.I,
-    )
-    police_image = models.ImageField(upload_to = user_directory_path, default='static/logo.jpeg')
+    police_id = models.CharField(max_length=50, primary_key = True,
+        verbose_name='ID')
+    police_name_english = models.CharField(max_length=200,
+        verbose_name='Name in English')
+    police_name_bangla = models.CharField(max_length=200,
+        verbose_name='Name in Bangla')
+    police_rank = models.CharField(max_length=4, choices=PoliceRanks.choices, default=PoliceRanks.I,
+        verbose_name='Rank')
+    police_image = models.ImageField(upload_to = user_directory_path, default='logo.jpeg',
+        verbose_name='Picture')
+    police_dob = models.DateField(default=get_default_my_date,
+        verbose_name='Date of Birth')
+    police_created = models.DateTimeField(auto_now_add = True)
+    police_updated = models.DateTimeField(auto_now = True)
     def __str__(self):
         return self.police_name_english
